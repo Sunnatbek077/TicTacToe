a279e2ade0cd3944aa611f0e106c314d7ba9299e
Sunnatbek077 <sunnatbekabdunabiyev@icloud.com>
2025-09-05 19:42:09 +0500

    Create system files and make SquareCell

---
+++ TicTacToe/Views/GameBoardView.swift
@@ -0,0 +1,20 @@
+//
+//  GameBoardView.swift
+//  TicTacToe
+//
+//  Created by Sunnatbek on 05/09/25.
+//
+
+import SwiftUI
+
+struct GameBoardView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    GameBoardView()
+}
---
+++ TicTacToe/Views/SelectionModeView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectionModeView.swift
+//  TicTacToe
+//
+//  Created by Sunnatbek on 05/09/25.
+//
+
+import SwiftUI
+
+struct SelectionModeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectionModeView()
+}
