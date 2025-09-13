47386de322fcc0e91d9239f5477dc0a3a4b309ea
Sunnatbek077 <sunnatbekabdunabiyev@icloud.com>
2025-09-13 15:57:43 +0500

    Building GameLogic

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -6,8 +6,36 @@
 //
 
 import SwiftUI
+import Combine
+import UIKit
+import Foundation
+
+class ViewModel: ObservableObject {
+    @Published var gameOver: Bool = false
+    @Published var winner: SquareStatus = .empty
+}
 
 struct ContentView: View {
+    // Make Trigger feedback with Taptic engine
+    static func triggerHapticFeedback(type: Int, override: Bool = false) {
+        let vibration = Foundation.UserDefaults.standard.value(forKey: "vibration") as? Bool
+        if vibration == true || override == true {
+            if type == 1 {
+                let generator = UIImpactFeedbackGenerator(style: .soft)
+                generator.impactOccurred()
+            } else if type == 2 {
+                let generator = UIImpactFeedbackGenerator(style: .medium)
+                generator.impactOccurred()
+            } else if type == 3 {
+                let generator = UIImpactFeedbackGenerator(style: .rigid)
+                generator.impactOccurred()
+            } else if type == 4 {
+                let generator = UIImpactFeedbackGenerator(style: .rigid)
+                generator.impactOccurred()
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
--- TicTacToe/GameLogicModel.swift
+++ TicTacToe/GameLogicModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import SwiftUI
 
 // MARK: -Skeleton map of the Board
 struct Board {
@@ -100,6 +101,145 @@ struct Board {
             }
         }
         return bestMove
+    }
+}
+
+class TicTacToeModel: ObservableObject {
+    @Published var squares = [Square]()
+    @Published var playerToMove: Bool = false
+    @ObservedObject var viewModel: ViewModel
+    
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        for _ in 0...8 {
+            squares.append(Square(status: .empty))
+        }
+    }
+    
+    func resetGame() -> Void {
+        for i in 0...8 {
+            squares[i].squareStatus = .empty
+            playerToMove = false
+        }
+    }
+    
+    var gameOver: (SquareStatus, Bool) {
+        get {
+            if viewModel.gameOver == false {
+                if winner.0 != .empty {
+                    colorize(check: winner.0, row: winner.1)
+                    viewModel.winner = winner.0
+                    return (winner.0, true)
+                } else {
+                    for i in 0...8 {
+                        if squares[i].squareStatus == .empty {
+                            return (.empty, false)
+                        }
+                        viewModel.gameOver = true
+                        return (.empty, true)
+                    }
+                }
+            }
+            return (.empty, false)
+        }
+    }
+    
+    func colorize(check: SquareStatus, row: [Int]) {
+        withAnimation {
+            if check == .x {
+                squares[row[0]].squareStatus = .xw
+                squares[row[1]].squareStatus = .xw
+                squares[row[2]].squareStatus = .xw
+            } else {
+                squares[row[0]].squareStatus = .ow
+                squares[row[1]].squareStatus = .ow
+                squares[row[2]].squareStatus = .ow
+            }
+        }
+        viewModel.gameOver = true
+    }
+    
+    func makeMove(index: Int, gameType: Bool) -> Bool {
+        var player: SquareStatus
+        if playerToMove == false {
+            player = .x
+        } else {
+            player = .o
+        }
+        if squares[index].squareStatus == .empty {
+            squares[index].squareStatus = player
+            if playerToMove == false && gameType == false && gameOver.1 == false {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.moveAI()
+                    ContentView.triggerHapticFeedback(type: 2)
+                    _ = self.gameOver
+                }
+            }
+            playerToMove.toggle()
+            _ = self.gameOver
+            return true
+        }
+        return false
+    }
+    
+    var getBoard: [SquareStatus] {
+        var moves: Array = [SquareStatus]()
+        for i in 0...8 {
+            moves.append(squares[i].squareStatus)
+        }
+        return moves
+    }
+    
+    private func moveAI() {
+        let boardMoves: [SquareStatus] = getBoard
+        let testBoard: Board = Board(position: boardMoves, turn: .o, lastMove: -1)
+        // Unwrap optional best move; if none, bail out
+        guard let answer = testBoard.findBestMove(testBoard) else { return }
+        playerToMove = true
+        _ = makeMove(index: answer, gameType: true)
+        
+    }
+    
+    private var winner: (SquareStatus, [Int]) {
+        get {
+            if let check = self.checkIndexes([0, 1, 2]) {
+                return (check, [0, 1, 2])
+            } else if let check = self.checkIndexes([3, 4, 5]) {
+                return (check, [3, 4, 5])
+            } else if let check = self.checkIndexes([6, 7, 8]) {
+                return (check, [6, 7, 8])
+            } else if let check = self.checkIndexes([0, 3, 6]) {
+                return (check, [0, 3, 6])
+            } else if let check = self.checkIndexes([1, 4, 7]) {
+                return (check, [1, 4, 7])
+            } else if let check = self.checkIndexes([2, 5, 8]) {
+                return (check, [2, 5, 8])
+            } else if let check = self.checkIndexes([0, 4, 8]) {
+                return (check, [0, 4, 8])
+            } else if let check = self.checkIndexes([2, 4, 6]) {
+                return (check, [2, 4, 6])
+            }
+            return (.empty, [])
+        }
+    }
+    
+    private func checkIndexes(_ indexes : [Int]) -> SquareStatus? {
+        var xCount : Int = 0
+        var oCount : Int = 0
+        for index in indexes {
+            let square = squares[index]
+            if square.squareStatus == .x || square.squareStatus == .xw {
+                xCount += 1
+            } else if square.squareStatus == .o || square.squareStatus == .ow {
+                oCount += 1
+            }
+        }
+        if xCount == 3 {
+            return .x
+        } else if oCount == 3 {
+            return .o
+        }
+        return nil
     }
 }
 
